7a2a2303a6f9c53c208b2e1245771d102af986ce
Julian Guarin <julian.guarin@fitech.com.co>
2018-10-18 07:07:47 -0500

    Added compile definitions

---
+++ m/CMakeFiles/SDL_image.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_WIC.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_WIC.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_bmp.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_bmp.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_gif.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_gif.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_jpg.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_jpg.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_lbm.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_lbm.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_pcx.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_pcx.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_png.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_png.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_pnm.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_pnm.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_svg.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_svg.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_tga.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_tga.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_tif.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_tif.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_webp.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_webp.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_xcf.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_xcf.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_xpm.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_xpm.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_xv.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_xv.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/IMG_xxx.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/IMG_xxx.c.o"
+  "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/showimage.c" "/Users/julianguarin/Code/hack/c++/sdl/SDL_image/m/CMakeFiles/SDL_image.dir/showimage.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "LOAD_BMP"
+  "LOAD_GIF"
+  "LOAD_JPG"
+  "LOAD_LBM"
+  "LOAD_PCX"
+  "LOAD_PNG"
+  "LOAD_PNM"
+  "LOAD_SVG"
+  "LOAD_TGA"
+  "LOAD_TIF"
+  "LOAD_WEBP"
+  "LOAD_XPM"
+  "LOAD_XV"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../SDL/include"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
